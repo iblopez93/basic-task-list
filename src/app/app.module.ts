ab74f45233cf83c552303f039e8b597c1e88b90d
iblopez93 <69406367+iblopez93@users.noreply.github.com>
2025-01-22 22:51:27 -0300

    Add imports

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+  ],
+  bootstrap: [ ],
+})
+export class AppModule { }
---
+++ src/app/taslist/taslist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taslist',
+  imports: [],
+  templateUrl: './taslist.component.html',
+  styleUrl: './taslist.component.css'
+})
+export class TaslistComponent {
+
+}
